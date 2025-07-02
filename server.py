50e0e85da314979268376813fe0c14b5f2a50e48
Supreetha <iris.0287@gmail.com>
2025-07-01 20:07:31 -0400

    Server file added

--- server.py
+++ server.py
@@ -1,4 +1,24 @@
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask()
+app = Flask("Emotion Detection")
+
+@app.route("/")
+def render_homepage():
+    return render_template("index.html")
+
+@app.route('/emotionDetector', methods=["GET"])
+def emotion_detection_analysis() -> str:
+    text = request.args["textToAnalyze"]
+    result = emotion_detector(text)
+    response = "For the given statement, the system response is "
+    response += "'anger'" + ": " + str(result['anger']) + ", "
+    response += " 'disgust'" + ": " + str(result['disgust']) + ", "
+    response += " 'fear'"  + ": " + str(result['fear']) + ", "
+    response += " 'joy'" + ": " + str(result['joy']) + ", "
+    response += " and 'sadness'" + ": " + str(result['sadness']) + "."
+    response += "The dominant emotion is " + result['dominant_emotion']
+    return response
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
